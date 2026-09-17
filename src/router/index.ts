d9f2a569cb513b1a2b03dd548774b1da84384a41
lindayun0331 <lindayun0331@gmail.com>
2026-09-17 10:29:49 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,48 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import('@/views/home/index.vue'),
-    },
-    {
-        path: '/english-literacy-course',
-        name: 'EnglishLiteracyCourse',
-        component: () => import('@/views/englishLiteracyCourse/index.vue'),
-    },
-    {
-        path: '/level-examination',
-        name: 'LevelExamination',
-        component: () => import('@/views/levelExamination/index.vue'),
-    },
-    {
-        path: '/insert-class',
-        name: 'InsertClass',
-        component: () => import('@/views/insertClass/index.vue'),
-    },
-    {
-        path: '/studying-abroad',
-        name: 'StudyingAbroad',
-        component: () => import('@/views/studyingAbroad/index.vue'),
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/home/index.vue"),
+  },
+  {
+    path: "/english-literacy-course",
+    name: "EnglishLiteracyCourse",
+    component: () => import("@/views/englishLiteracyCourse/index.vue"),
+  },
+  {
+    path: "/level-examination",
+    name: "LevelExamination",
+    component: () => import("@/views/levelExamination/index.vue"),
+  },
+  {
+    path: "/insert-class",
+    name: "InsertClass",
+    component: () => import("@/views/insertClass/index.vue"),
+  },
+  {
+    path: "/studying-abroad",
+    name: "StudyingAbroad",
+    component: () => import("@/views/studyingAbroad/index.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-    scrollBehavior(to, _from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition;
-        }
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
 
-        if (to.hash) {
-            return { el: to.hash };
-        }
+    if (to.hash) {
+      return { el: to.hash };
+    }
 
-        return { top: 0 };
-    },
+    return { top: 0 };
+  },
 });
 
 export default router;
